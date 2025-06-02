51bb193cb88f40a6b781a2c532c61c9a9a4c5502
Ktiora1 <guilhermemarques3354@gmail.com>
2025-06-01 21:50:58 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -27,5 +27,8 @@ def index():
         resultado = calcular_sensacao_termica(temp, vel)
     return render_template('index.html', resultado=resultado)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
